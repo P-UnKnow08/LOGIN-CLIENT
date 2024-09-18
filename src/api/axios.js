ea3d911c90ac91290576301893490058db5ab4c0
Miguel Acevedo <acebedo2524@gmail.com>
2024-09-18 16:04:14 -0500

    ForntEnd Ready

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// Utilizamos la URL base definida en el archivo .env
 const instance = axios.create({
-    baseURL: "http://localhost:4000/api",
-    withCredentials: true,
-})
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,  // VITE_API_URL se define en el archivo .env
+  withCredentials: true,  // Esto permite que las cookies se envíen en las solicitudes
+});
 
 export default instance;
